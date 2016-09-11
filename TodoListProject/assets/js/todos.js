69696b239a8f6fe5a6c2802e1ca816612c94baf6
andysowar <andy.sowar@gmail.com>
2016-09-11 12:42:02 -0600

    Adding ToDo List

---
+++ TodoListProject/assets/js/todos.js
@@ -0,0 +1,25 @@
+// Check off To-Dos by clicking
+$('ul').on("click", "li", function(){
+	$(this).toggleClass("deleted");
+});
+
+$('ul').on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		// grabbing new To-Do
+		var newTodo = $(this).val();
+		$(this).val("");
+		// create new 'li'
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newTodo + "</li>");
+	}
+})
+
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
+})
