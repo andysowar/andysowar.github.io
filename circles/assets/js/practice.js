1b7a4cb1bda29ff62d1a2c5be57795e83bf9896e
andysowar <andy.sowar@gmail.com>
2016-09-24 10:48:47 -0600

    Adding Circle Beats Project

---
+++ circles/assets/js/practice.js
@@ -0,0 +1,11 @@
+var balls = [];
+var numBalls = 100;
+for (var i = 0; i < numBalls; i++) {
+	var position = Point.random() * view.size;
+	var vector = new Point({
+		angle: 360 * Math.random(),
+		length: Math.random() * 10
+	});
+	var radius = Math.random() * 60 + 60;
+	balls.push(new Ball(radius, position, vector));
+}
